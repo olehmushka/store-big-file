9e5475db2d9686b4d4dd2af47f3e198deda7cbd9
Oleh Mushka <olegamysk@gmail.com>
2020-07-15 13:05:30 +0300

    added husky & prettier & refactored

--- split-csv-function/src/index.ts
+++ split-csv-function/src/index.ts
@@ -1,4 +1,3 @@
-
 import { Storage } from '@google-cloud/storage';
 import { PubSub } from '@google-cloud/pubsub';
 import { Context } from '@google-cloud/functions-framework/build/src/functions';
@@ -7,7 +6,6 @@ import config from './config';
 
 const csvSplitStream = require('csv-split-stream');
 
-
 type BaseFunctionInputData = {};
 
 interface ISplitCSVDataInput extends BaseFunctionInputData {
@@ -15,40 +13,37 @@ interface ISplitCSVDataInput extends BaseFunctionInputData {
   name: string;
 }
 
-export const splitCsvFunction = (file: ISplitCSVDataInput, context: Context, callback: Function): void => {
+export const splitCsvFunction = async (
+  file: ISplitCSVDataInput,
+  context: Context,
+  callback: Function,
+): Promise<void> => {
   if (!file.name.includes('input')) {
     return callback(null, null);
   }
-  const storageClient = new Storage()
+  const storageClient = new Storage();
   const pubSubClient = new PubSub();
   const timestamp = Date.now();
   const fullTopicName = `projects/${config.PROJECT_ID}/topics/${config.SPLITTED_CSV_FILE_TOPIC_NAME}`;
   const outputFilenames: string[] = [];
 
   const readFileStream = storageClient.bucket(file.bucket).file(file.name).createReadStream();
-  csvSplitStream.split(readFileStream, { lineLimit: 100 },
-    (index: number) => {
+  try {
+    await csvSplitStream.split(readFileStream, { lineLimit: 100 }, (index: number) => {
       const outputFilename = `output-${timestamp}-${index}.csv`;
       outputFilenames.push(outputFilename);
 
       return storageClient.bucket(file.bucket).file(outputFilename).createWriteStream();
-    }
-  )
-    .then(async () => {
-      return await Promise.all(
-        outputFilenames.map((filename) => {
-          const payload = JSON.stringify({ data: { csvFilename: filename } });
-
-          return pubSubClient
-            .topic(fullTopicName)
-            .publish(Buffer.from(payload));
-        }),
-      );
-    })
-    .then((publishedMessageIDs: string[]) => {
-      callback(null, { success: true, publishedMessageIDs });
-    })
-    .catch((error: unknown) => {
-      callback(error, { success: false });
     });
+    const publishedMessageIDs = await Promise.all(
+      outputFilenames.map((filename) => {
+        const payload = JSON.stringify({ data: { csvFilename: filename } });
+
+        return pubSubClient.topic(fullTopicName).publish(Buffer.from(payload));
+      }),
+    );
+    callback(null, { success: true, publishedMessageIDs });
+  } catch (error) {
+    callback(error, { success: false });
+  }
 };
